815343ed873a6bf6c38f3025d8dac15b7f98cfe9
K0ntact <duyan5745@gmail.com>
2023-11-05 20:15:59 +0700

    refactor: move upload section to a separate fragment

--- app/src/main/java/vn/edu/usth/mobile_app/MainActivity.kt
+++ app/src/main/java/vn/edu/usth/mobile_app/MainActivity.kt
@@ -1,28 +1,28 @@
 package vn.edu.usth.mobile_app
 
-import android.content.ContentResolver
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.OpenableColumns
 import android.view.MenuItem
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import vn.edu.usth.mobile_app.ui.LoginActivity
+import vn.edu.usth.mobile_app.ui.UploadFragment
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var toggle: ActionBarDrawerToggle
+    private var isLogin = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (intent.extras != null) {
+            isLogin = intent.extras!!.getBoolean("isLogin")
+        }
 
         val toolbar = findViewById<Toolbar>(R.id.materialToolbar)
         setSupportActionBar(toolbar)
@@ -51,14 +51,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val modelList = resources.getStringArray(R.array.model_list)
-        val modelAdapter = ArrayAdapter(this, R.layout.simple_dropdown_item, modelList)
-        val autoCompleteModel = findViewById<AutoCompleteTextView>(R.id.autoCompleteModel)
-        autoCompleteModel.setAdapter(modelAdapter)
-
-        val fileChoose = findViewById<AutoCompleteTextView>(R.id.autoCompleteFilePath)
-        val getFile = getFileLauncher(fileChoose)
-        fileChoose.setOnClickListener { getFile.launch("application/sed") }
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, UploadFragment())
+        fragmentTransaction.commit()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -67,23 +63,4 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
-
-    private fun getFileLauncher(fileChoose: AutoCompleteTextView) =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-            if (uri == null) {
-                return@registerForActivityResult
-            }
-            val name = queryName(applicationContext.contentResolver, uri)
-            fileChoose.setText(name)
-        }
-
-    private fun queryName(resolver: ContentResolver, uri: Uri): String? {
-        val returnCursor = resolver.query(uri, null, null, null, null)!!
-        val nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-        returnCursor.moveToFirst()
-        val name = returnCursor.getString(nameIndex)
-        returnCursor.close()
-        return name
-    }
-
 }
