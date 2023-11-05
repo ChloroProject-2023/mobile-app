10eb63760ee8dac414d50ed628edd87e0960e8b3
K0ntact <duyan5745@gmail.com>
2023-11-05 20:16:25 +0700

    refactor: minor UI changes

--- app/src/main/java/vn/edu/usth/mobile_app/ui/LoginActivity.kt
+++ app/src/main/java/vn/edu/usth/mobile_app/ui/LoginActivity.kt
@@ -2,8 +2,10 @@ package vn.edu.usth.mobile_app.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import vn.edu.usth.mobile_app.MainActivity
 import vn.edu.usth.mobile_app.R
 
 class LoginActivity : AppCompatActivity() {
@@ -11,9 +13,18 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
-        val signup = findViewById<TextView>(R.id.buttonSignup)
+        val signup = findViewById<TextView>(R.id.button_login_signup)
         signup.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        val login = findViewById<Button>(R.id.button_login)
+        login.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtras(
+                Bundle().apply {putBoolean("isLogin", true)}
+            )
             startActivity(intent)
         }
     }
