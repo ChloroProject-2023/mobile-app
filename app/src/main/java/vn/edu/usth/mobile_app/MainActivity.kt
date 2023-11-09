bd56521a5037a73feff49fdc83e1ee3e4890238e
K0ntact <duyan5745@gmail.com>
2023-11-09 12:53:10 +0700

    refactor: remove Navigation Drawer, add Bottom Navigation

--- app/src/main/java/vn/edu/usth/mobile_app/MainActivity.kt
+++ app/src/main/java/vn/edu/usth/mobile_app/MainActivity.kt
@@ -2,19 +2,18 @@ package vn.edu.usth.mobile_app
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import vn.edu.usth.mobile_app.ui.LoginActivity
 import vn.edu.usth.mobile_app.ui.UploadFragment
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var toggle: ActionBarDrawerToggle
     private var isLogin = false
+    private val fragmentManager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,40 +26,68 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.materialToolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
-        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-        val navigationView = findViewById<NavigationView>(R.id.navigationView)
-        navigationView.setNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.navLogin -> {
-                    val loginActivity = Intent(this, LoginActivity::class.java)
-                    startActivity(loginActivity)
+
+        replaceFragment(UploadFragment())
+
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomBar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navUpload -> {
+                    replaceFragment(UploadFragment())
                     true
                 }
+
                 R.id.navHistory -> {
-                    drawerLayout.close()
+                    if (!isLogin) {
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
                     true
                 }
+
                 R.id.navSettings -> {
-                    drawerLayout.close()
                     true
                 }
                 else -> false
             }
         }
+        bottomBar.setOnItemReselectedListener {}
 
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container, UploadFragment())
-        fragmentTransaction.commit()
+//        // Set the listener for both navigation bar and navigation drawer
+//        // Doesn't work ???
+
+//        NavigationBarView.OnItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.navUpload -> {
+//                    true
+//                }
+//                R.id.navHistory -> {
+//                    println("\n\n\nHistory\n\n\n")
+//                    if (!isLogin) {
+//                        println("\n\n\nNot logged in!\n\n\n")
+//                        val intent = Intent(this, LoginActivity::class.java)
+//                        startActivity(intent)
+//                    }
+//                    true
+//                }
+//                R.id.navSettings -> {
+//                  true
+//                }
+//                else -> false
+//            }
+//        }
+//        NavigationBarView.OnItemReselectedListener { item ->
+//            when (item.itemId) {
+//                R.id.navUpload -> {}
+//                R.id.navHistory -> {}
+//                R.id.navSettings -> {}
+//            }
+//        }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)) {
-            return true
-        }
-        return super.onOptionsItemSelected(item)
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
