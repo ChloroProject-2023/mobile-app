290abb5ed827f602dcfc48709522553bc844c67b
K0ntact <duyan5745@gmail.com>
2023-11-16 11:15:53 +0700

    fix: text input not showing error correctly

--- app/src/main/java/vn/edu/usth/mobile_app/ui/LoginActivity.kt
+++ app/src/main/java/vn/edu/usth/mobile_app/ui/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.view.MotionEvent
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 import vn.edu.usth.mobile_app.MainActivity
 import vn.edu.usth.mobile_app.R
 
@@ -13,6 +14,12 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        val usernameField = findViewById<TextInputLayout>(R.id.textInputLayout_login_username)
+        val passwordField = findViewById<TextInputLayout>(R.id.textInputLayout_login_password)
+
+        val username = findViewById<TextView>(R.id.editText_login_username)
+        val password = findViewById<TextView>(R.id.editText_login_password)
 
         val signup = findViewById<TextView>(R.id.button_login_signup)
         signup.setOnClickListener {
@@ -22,6 +29,18 @@ class LoginActivity : AppCompatActivity() {
 
         val login = findViewById<Button>(R.id.button_login)
         login.setOnClickListener {
+            usernameField.error = null
+            passwordField.error = null
+
+            if (username.text.toString().isEmpty()){
+                usernameField.error = "Username is required"
+                return@setOnClickListener
+            }
+            if (password.text.toString().isEmpty()){
+                passwordField.error = "Password is required"
+                return@setOnClickListener
+            }
+
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtras(
                 Bundle().apply {
--- app/src/main/java/vn/edu/usth/mobile_app/ui/SignupActivity.kt
+++ app/src/main/java/vn/edu/usth/mobile_app/ui/SignupActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
 import vn.edu.usth.mobile_app.MainActivity
 import vn.edu.usth.mobile_app.R
 
@@ -15,11 +16,17 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        val fnField = findViewById<TextView>(R.id.editText_signup_firstName)
-        val lnField = findViewById<TextView>(R.id.editText_signup_lastName)
-        val usernameField = findViewById<TextView>(R.id.editText_signup_username)
-        val passwordField = findViewById<TextView>(R.id.editText_signup_password)
-        val confirmPasswordField = findViewById<TextView>(R.id.editText_signup_passCf)
+        val fnField = findViewById<TextInputLayout>(R.id.textInputLayout_signup_fnField)
+        val lnField = findViewById<TextInputLayout>(R.id.textInputLayout_signup_lnField)
+        val usernameField = findViewById<TextInputLayout>(R.id.textInputLayout_signup_usernameField)
+        val passwordField = findViewById<TextInputLayout>(R.id.textInputLayout_signup_pwField)
+        val confirmPasswordField = findViewById<TextInputLayout>(R.id.textInputLayout_signup_cfField)
+
+        val firstname = findViewById<TextView>(R.id.editText_signup_firstName)
+        val lastname = findViewById<TextView>(R.id.editText_signup_lastName)
+        val username = findViewById<TextView>(R.id.editText_signup_username)
+        val password = findViewById<TextView>(R.id.editText_signup_password)
+        val confirmPassword = findViewById<TextView>(R.id.editText_signup_passCf)
 
         val backButton = findViewById<MaterialButton>(R.id.button_signup_back)
         backButton.setOnClickListener {
@@ -28,28 +35,32 @@ class SignupActivity : AppCompatActivity() {
 
         val signup = findViewById<Button>(R.id.button_signup)
         signup.setOnClickListener {
+            fnField.error = null
+            lnField.error = null
+            usernameField.error = null
+            passwordField.error = null
+            confirmPasswordField.error = null
 
-            if (fnField.text.toString().isEmpty()){
+            if (firstname.text.toString().isEmpty()){
                 fnField.error = "First name is required"
                 return@setOnClickListener
             }
-            if (lnField.text.toString().isEmpty()){
+            if (lastname.text.toString().isEmpty()){
                 lnField.error = "Last name is required"
                 return@setOnClickListener
             }
-            if (usernameField.text.toString().isEmpty()){
+            if (username.text.toString().isEmpty()){
                 usernameField.error = "Username is required"
                 return@setOnClickListener
             }
-            if (passwordField.text.toString().isEmpty()){
+            if (password.text.toString().isEmpty()){
                 passwordField.error = "Password is required"
                 return@setOnClickListener
             }
-            if (confirmPasswordField.text.toString().isEmpty()){
+            if (confirmPassword.text.toString().isEmpty()){
                 confirmPasswordField.error = "Please confirm your password"
                 return@setOnClickListener
             }
-
 
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtras(
