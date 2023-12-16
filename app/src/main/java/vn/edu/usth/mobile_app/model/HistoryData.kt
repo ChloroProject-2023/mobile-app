68b3695f836ba9f9a7a7c33532649a41c56a7e68
K0ntact <duyan5745@gmail.com>
2023-12-16 10:27:54 +0700

    fix: missing ids

--- app/src/main/java/vn/edu/usth/mobile_app/model/HistoryData.kt
+++ app/src/main/java/vn/edu/usth/mobile_app/model/HistoryData.kt
@@ -1,6 +1,7 @@
 package vn.edu.usth.mobile_app.model
 
 data class HistoryData(
+    var id: Int = -1,
     var modelName: String = "",
     var date: Long = 1,
     var uploadedDataPath: String = "",
