e9cfbe2a32871158a557125cc062dc1bb191ef18
K0ntact <duyan5745@gmail.com>
2023-12-25 20:49:18 +0700

    perf(AdminAnalytics): async inflate for aachartview

--- app/src/main/java/vn/edu/usth/mobile_app/ui/admin/AdminAnalyticsFragment.kt
+++ app/src/main/java/vn/edu/usth/mobile_app/ui/admin/AdminAnalyticsFragment.kt
@@ -1,9 +1,11 @@
 package vn.edu.usth.mobile_app.ui.admin
 
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.asynclayoutinflater.view.AsyncLayoutInflater
 import androidx.fragment.app.Fragment
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
@@ -18,9 +20,6 @@ class AdminAnalyticsFragment : Fragment(R.layout.fragment_admin_analytics) {
     private val binding get() = _binding!!
     private val viewModel: FragmentAdminAnalyticsViewModel by viewModels()
 
-    private lateinit var requestFreqAAChart: AAChartView
-    private lateinit var top5ModelsAAChart: AAChartView
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,8 +28,24 @@ class AdminAnalyticsFragment : Fragment(R.layout.fragment_admin_analytics) {
         _binding = FragmentAdminAnalyticsBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        requestFreqAAChart = binding.chartViewAdminAnalyticsRequestFrequency
-        top5ModelsAAChart = binding.chartViewAdminAnalyticsTop5Models
+        val requestFreqFrameLayout = binding.frameLayoutAdminAnalyticsRequestFrequency
+        val top5ModelsFrameLayout = binding.frameLayoutAdminAnalyticsTop5Models
+        val requestFreqProcessBar = binding.progressBarAdminAnalyticsRequestFrequency
+        val top5ModelsProcessBar = binding.progressBarAdminAnalyticsTop5Models
+
+        val requestFreqAsyncInflater = AsyncLayoutInflater(requireContext())
+        requestFreqAsyncInflater.inflate(R.layout.fragment_chart, requestFreqFrameLayout) { view1, _, _ ->
+            drawRequestFreqChart(view1)
+            requestFreqProcessBar.visibility = View.GONE
+            requestFreqFrameLayout.addView(view1)
+        }
+
+        val top5ModelsAsyncInflater = AsyncLayoutInflater(requireContext())
+        top5ModelsAsyncInflater.inflate(R.layout.fragment_chart, top5ModelsFrameLayout) { view1, _, _ ->
+            drawTop5ModelsChart(view1)
+            top5ModelsProcessBar.visibility = View.GONE
+            top5ModelsFrameLayout.addView(view1)
+        }
 
         binding.textViewAdminAnalyticsLast24hRequestValue.text = viewModel.last24hRequest.toString()
         binding.textViewAdminAnalyticsLast24hUsersValue.text = viewModel.last24hUsers.toString()
@@ -38,11 +53,19 @@ class AdminAnalyticsFragment : Fragment(R.layout.fragment_admin_analytics) {
         return view
     }
 
-    override fun onResume() {
-        super.onResume()
+    private fun drawRequestFreqChart(view: View){
+        val requestFreqAAChart = view.findViewById<AAChartView>(R.id.aaChartView_chartFragment)
+
+        val value = TypedValue()
+        context?.theme?.resolveAttribute(android.R.attr.colorPrimary, value, true)
+        val color = value.data
+        val primaryColor = String.format("#%06X", 0xFFFFFF and color)
+
         val requestFreqChartModel = AAChartModel()
             .chartType(AAChartType.Areaspline)
             .legendEnabled(false)
+            .backgroundColor("#00000000")  // Transparent
+            .colorsTheme(arrayOf(primaryColor))
             .yAxisTitle("Requests")
             .series(arrayOf(
                 AASeriesElement()
@@ -51,10 +74,21 @@ class AdminAnalyticsFragment : Fragment(R.layout.fragment_admin_analytics) {
             )
             )
         requestFreqAAChart.aa_drawChartWithChartModel(requestFreqChartModel)
+    }
+
+    private fun drawTop5ModelsChart(view: View){
+        val top5ModelsAAChart = view.findViewById<AAChartView>(R.id.aaChartView_chartFragment)
+
+        val value = TypedValue()
+        context?.theme?.resolveAttribute(android.R.attr.colorPrimary, value, true)
+        val color = value.data
+        val primaryColor = String.format("#%06X", 0xFFFFFF and color)
 
         val top5ModelsChartModel = AAChartModel()
             .chartType(AAChartType.Bar)
             .legendEnabled(false)
+            .backgroundColor("#00000000")  // Transparent
+            .colorsTheme(arrayOf(primaryColor))
             .categories(viewModel.top5ModelData.keys.toTypedArray())
             .yAxisTitle("Requests")
             .series(arrayOf(
