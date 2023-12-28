7b051e45e6348945461289464120050f5217cfc2
K0ntact <duyan5745@gmail.com>
2023-12-28 14:49:49 +0700

    feat: change graph package to vico graph

--- app/src/main/java/vn/edu/usth/mobile_app/ui/modeldetails/ModelDetailsViewModel.kt
+++ app/src/main/java/vn/edu/usth/mobile_app/ui/modeldetails/ModelDetailsViewModel.kt
@@ -1,6 +1,8 @@
 package vn.edu.usth.mobile_app.ui.modeldetails
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import vn.edu.usth.mobile_app.model.ModelData
 
 class ModelDetailsViewModel: ViewModel() {
@@ -8,29 +10,50 @@ class ModelDetailsViewModel: ViewModel() {
     private var _modelName: String = ""
     private var _author: String = ""
     private var _description: String = ""
-    private var _ratings: Map<String, Int> = emptyMap()
-    private var _mostUsedModels: List<ModelData> = emptyList()
-    private var _similarModels: List<ModelData> = emptyList()
+    val ratings = mutableMapOf<Int, Int>()
+    val mostUsedModels = mutableListOf<ModelData>()
+    val similarModels = mutableListOf<ModelData>()
 
     val modelId: Int get() = _modelID
     val modelName: String get() = _modelName
     val author: String get() = _author
     val description: String get() = _description
-    val ratings: Map<String, Int> get() = _ratings
-    val mostUsedModels: List<ModelData> get() = _mostUsedModels
-    val similarModels: List<ModelData> get() = _similarModels
 
     init {
+        viewModelScope.launch {
+            // Fetch model details from server
+            fetchModelDetails()
+        }
+
+        // Mock data
         _modelName = "NPK AI"
         _author = "USTH"
         _description = "This model is used to predict the NPK content of soil"
-        _ratings = mapOf(
-            "1" to 12,
-            "2" to 2,
-            "3" to 10,
-            "4" to 30,
-            "5" to 100,)
-        _mostUsedModels = listOf(1, 2, 3, 4, 5).map {
+
+        ratings.putAll(mapOf(
+            1 to 12,
+            2 to 2,
+            3 to 10,
+            4 to 30,
+            5 to 100,))
+
+        mostUsedModels.addAll(
+            listOf(1, 2, 3, 4, 5).map {
+                ModelData(
+                    id = it,
+                    name = "NPK AI",
+                    description = "This model is used to predict the NPK content of soil",
+                    modelPath = "",
+                    type = "AI",
+                    creatorName = "USTH",
+                    createdAt = 1,
+                    usage = 100
+                )
+            }
+        )
+
+        similarModels.addAll(
+            listOf(1, 2, 3, 4, 5).map {
             ModelData(
                 id = it,
                 name = "NPK AI",
@@ -41,19 +64,7 @@ class ModelDetailsViewModel: ViewModel() {
                 createdAt = 1,
                 usage = 100
             )
-        }
-        _similarModels = listOf(1, 2, 3, 4, 5).map {
-            ModelData(
-                id = it,
-                name = "NPK AI",
-                description = "This model is used to predict the NPK content of soil",
-                modelPath = "",
-                type = "AI",
-                creatorName = "USTH",
-                createdAt = 1,
-                usage = 100
-            )
-        }
+        })
     }
 
     fun setModelID(modelID: Int) {
