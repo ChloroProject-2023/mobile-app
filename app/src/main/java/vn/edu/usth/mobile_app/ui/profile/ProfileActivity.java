3b0a3ea0cf3a9a7d3e751dd1dccaac316d4b67c7
DuckNguyen210 <thanhduc27282104@gmail.com>
2023-12-30 16:47:53 +0700

    Add User Profile

--- app/src/main/java/vn/edu/usth/mobile_app/ui/profile/ProfileActivity.java
+++ app/src/main/java/vn/edu/usth/mobile_app/ui/profile/ProfileActivity.java
@@ -1,14 +1,10 @@
 package vn.edu.usth.mobile_app.ui.profile;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.palette.graphics.Palette;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -18,10 +14,18 @@ import vn.edu.usth.mobile_app.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        Toolbar toolbar = findViewById(R.id.profile_toolbar);
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.profile_collapse);
+
+        setSupportActionBar(toolbar);
+        collapsingToolbarLayout.setTitle(getTitle());
 
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         ViewPager viewPager = findViewById(R.id.profile_viewpager);
@@ -30,27 +34,14 @@ public class ProfileActivity extends AppCompatActivity {
         viewPager.setAdapter(viewPagerAdapter);
 
         tabLayout.setupWithViewPager(viewPager);
+
+        initToolbar();
     }
 
     private void initToolbar() {
-        Toolbar toolbar = findViewById(R.id.profile_toolbar);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-    }
-
-    private void setTabLayAnimation() {
-        final CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.profile_collapse);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.id.profile_expand);
-        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-            @Override
-            public void onGenerated(@Nullable Palette palette) {
-                int myColor = palette.getVibrantColor(getResources().getColor(R.color.md_theme_light_primary));
-                int myDarkColor = palette.getVibrantColor(getResources().getColor(R.color.black_trans));
-                collapsingToolbarLayout.setContentScrimColor(myColor);
-                collapsingToolbarLayout.setStatusBarScrimColor(myDarkColor);
-            }
-        });
     }
 }
