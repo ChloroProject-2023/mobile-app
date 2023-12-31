0e6a3c821aa92487bcd6960c504e944f2766ceee
bhhoang <canhchimcuajin@gmail.com>
2023-12-31 20:59:22 +0700

    feat: delete resource file

---
+++ app/src/main/java/vn/edu/usth/mobile_app/ui/usermenu/resourcelist/BottomSheet.kt
@@ -0,0 +1,17 @@
+package vn.edu.usth.mobile_app.ui.usermenu.resourcelist
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import vn.edu.usth.mobile_app.R
+
+class BottomSheet: BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.bottom_sheet_dialog, container, false)
+    }
+}
