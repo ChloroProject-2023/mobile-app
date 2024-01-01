0a82ad7a14e58c13724466f63cbc267e49c2fc3f
K0ntact <duyan5745@gmail.com>
2024-01-01 10:02:24 +0700

    refactor: change to from java to kotlin

---
+++ app/src/main/java/vn/edu/usth/mobile_app/ui/profile/ProfileActivity.kt
@@ -0,0 +1,17 @@
+package vn.edu.usth.mobile_app.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import vn.edu.usth.mobile_app.databinding.ActivityUserProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+    private var _binding: ActivityUserProfileBinding? = null
+    private val binding get() = _binding!!
+
+    private val toolbar: Toolbar? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
